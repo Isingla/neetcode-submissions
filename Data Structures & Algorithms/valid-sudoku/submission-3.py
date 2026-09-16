a3c9611c5ec725ec1ad8abc1903dd796d8568389
Ishaan Singla <64155231+Isingla@users.noreply.github.com>
2026-09-16 09:00:12 -0400

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        colDict = defaultdict(set)
+        boxDict = defaultdict(set)
+        for r in range(len(board)):
+            filteredRow = set()
+            for c in range(len(board[r])):
+                if board[r][c] != ".":
+                    if board[r][c] in filteredRow or board[r][c] in colDict[c] or board[r][c] in boxDict[r//3, c//3]:
+                        return False
+                    else:
+                        filteredRow.add(board[r][c])
+                        colDict[c].add(board[r][c])
+                        boxDict[r//3, c//3].add(board[r][c])
+        return True
